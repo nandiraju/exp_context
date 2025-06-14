1c1939c93fbd0f9faf28085d7d0f63a02e63795e
Srikanth <nandiraju@yahoo.com>
2025-06-13 23:57:24 -0700

    Notes

---
+++ app/components/NotesList.tsx
@@ -0,0 +1,66 @@
+import { FlatList, Pressable, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { useNotes } from "../context/NotesContext";
+import { Alert } from "react-native";
+
+const NotesList = () => {
+  const { notes, deleteNote } = useNotes();
+  return (
+    <View
+      style={{
+        flex: 1,
+        padding: 10,
+        backgroundColor: "dodgerblue",
+        borderRadius: 20,
+      }}
+    >
+      <FlatList
+        data={notes}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "space-between",
+              paddingVertical: 8,
+            }}
+          >
+            <Text>
+              {item.id} - {item.title}
+            </Text>
+            <Pressable
+              onPress={() => {
+                console.log("Delete pressed for", item.id);
+                Alert.alert(
+                  "Delete Note",
+                  "Are you sure you want to delete this note?",
+                  [
+                    {
+                      text: "Cancel",
+                      style: "cancel",
+                    },
+                    {
+                      text: "Delete",
+                      style: "destructive",
+                      onPress: () => deleteNote(item.id),
+                    },
+                  ],
+                  { cancelable: true }
+                );
+                return;
+                deleteNote(item.id);
+              }}
+            >
+              <Text style={{ color: "red", marginRight: 16 }}>Delete</Text>
+            </Pressable>
+          </View>
+        )}
+      />
+    </View>
+  );
+};
+
+export default NotesList;
+
+const styles = StyleSheet.create({});
