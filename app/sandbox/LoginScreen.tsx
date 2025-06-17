65438340272e6f9c3e0d99eb5559bab4f52d48da
Srikanth <nandiraju@yahoo.com>
2025-06-16 18:40:08 -0700

    Lot of stuff

--- app/sandbox/LoginScreen.tsx
+++ app/sandbox/LoginScreen.tsx
@@ -1,26 +1,31 @@
-import { View, Text } from "react-native";
+import { View, Text, KeyboardAvoidingView, Platform } from "react-native";
 import React from "react";
 import IconInput from "../components/IconInput";
 
 const LoginScreen = () => {
   return (
-    <View className="flex-1 justify-center items-center bg-blue-50 gap-2">
-      <Text>LoginScreen</Text>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      className="flex-1"
+    >
+      <View className="flex-1 justify-center items-center bg-snow gap-2">
+        <Text>LoginScreen</Text>
 
-      <IconInput
-        placeholder="Email"
-        iconName="mail"
-        iconSize={20}
-        style={{ width: "80%", height: 50 }}
-      />
-      <IconInput
-        placeholder="Email"
-        iconName="security"
-        iconSize={20}
-        r
-        style={{ width: "80%", height: 50 }}
-      />
-    </View>
+        <IconInput
+          placeholder="Email"
+          iconName="mail"
+          iconSize={20}
+          style={{ width: "80%", height: 50 }}
+        />
+        <IconInput
+          placeholder="Email"
+          iconName="security"
+          iconSize={20}
+          r
+          style={{ width: "80%", height: 50 }}
+        />
+      </View>
+    </KeyboardAvoidingView>
   );
 };
 
--- metro.config.js
+++ metro.config.js
@@ -2,5 +2,6 @@ const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
+// DEFAULT_LOGGER_CONFIG.resolver.sourceExts.push("cjs");
 
 module.exports = withNativeWind(config, { input: "./global.css" });
