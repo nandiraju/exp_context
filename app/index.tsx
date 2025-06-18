9e54388693fb2e1f9eb4c762ed237ea90f60de82
Srikanth <nandiraju@yahoo.com>
2025-06-17 23:43:36 -0700

    Status bar fix

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
+import { SafeAreaView } from "react-native-safe-area-context";
 import SignInScreen from "./(auth)/SignIn";
 import NetworkChecker from "./(auth)/NetworkChecker";
 
 export default function Index() {
   return (
-    <>
+    <SafeAreaView style={{ flex: 1 }}>
       <StatusBar style="auto" />
       <SignInScreen />
       <NetworkChecker />
-    </>
+    </SafeAreaView>
   );
 }
