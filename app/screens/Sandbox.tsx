319fdc6a2550b412ecc94ee4bb74797ccfe49f3a
Srikanth-promax <nandiraju@yahoo.com>
2025-06-22 20:46:14 -0700

    Divider

--- app/screens/Sandbox.tsx
+++ app/screens/Sandbox.tsx
@@ -1,8 +1,15 @@
-import { StyleSheet, View, ScrollView, TouchableOpacity } from "react-native";
+import {
+  StyleSheet,
+  View,
+  ScrollView,
+  TouchableOpacity,
+  Text,
+} from "react-native";
 import React, { use, useEffect } from "react";
 import { NewsCard } from "./NewsCard";
 import OSakhiCard from "../components/OsakhiCard";
 import MenuScreen from "./MenuScreen";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function Sandbox() {
   const handleCardPress = (data: any) => {
@@ -11,11 +18,28 @@ export default function Sandbox() {
 
   return (
     <ScrollView className="bg-snow-50">
+      <HeadingDivider iconName="albums-outline" title="Interact with OSakhi" />
       <OSakhiCard />
+      <HeadingDivider iconName="albums-outline" title="start" />
       <MenuScreen />
+      <HeadingDivider iconName="albums-outline" title="Trending in cancer" />
       <NewsCard />
     </ScrollView>
   );
 }
 
-const styles = StyleSheet.create({});
+export const HeadingDivider = ({ iconName, title, hideRightIcon }: any) => {
+  return (
+    <>
+      <View className="flex-row items-center my-4 justify-between px-4">
+        <View className="flex-row items-start gap-3 ">
+          <Ionicons name={iconName} size={24} color="dodgerblue" />
+          <Text className="text-lg font-bold">{title}</Text>
+        </View>
+        {!hideRightIcon && (
+          <Ionicons name="arrow-forward-outline" size={24} color="dodgerblue" />
+        )}
+      </View>
+    </>
+  );
+};
